316f5bc218d176f0940b18131ef784ad2cbc92c5
MrWakeCZ <joseff.kral@gmail.com>
2017-04-27 02:17:18 +0200

    Dočasné zablokování soukromých zpráv

--- src/main/java/cz/wake/corgibot/listener/MainListener.java
+++ src/main/java/cz/wake/corgibot/listener/MainListener.java
@@ -5,48 +5,63 @@ import cz.wake.corgibot.commands.Command;
 import cz.wake.corgibot.commands.CommandType;
 import cz.wake.corgibot.utils.MessageUtils;
 import net.dv8tion.jda.core.Permission;
+import net.dv8tion.jda.core.entities.ChannelType;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.Role;
 import net.dv8tion.jda.core.entities.User;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+import java.awt.*;
 
 public class MainListener extends ListenerAdapter {
 
     @Override
-    public void onGuildMessageReceived(GuildMessageReceivedEvent e) {
-        if (e.getMessage().getRawContent().startsWith(String.valueOf(CorgiBot.PREFIX))
-                && !e.getAuthor().isBot()) {
-            String message = e.getMessage().getRawContent();
-            String command = message.substring(1);
-            String[] args = new String[0];
-            if (message.contains(" ")) {
-                command = command.substring(0, message.indexOf(" ") - 1);
+    public void onMessageReceived(MessageReceivedEvent e) {
 
-                args = message.substring(message.indexOf(" ") + 1).split(" ");
-            }
-            for (Command cmd : CorgiBot.getInstance().getCommandHandler().getCommands()) {
-                if (cmd.getCommand().equalsIgnoreCase(command)) {
-                    String[] finalArgs = args;
-                    if (cmd.getType() == CommandType.WAKE) {
-                        if (!isCreator(e.getMessage().getAuthor())) {
-                            return;
+        if(e.getAuthor().isBot()){
+            return;
+        }
+
+        //TODO: Dodělat, typy příkazů podle channelu
+
+        if(!e.isFromType(ChannelType.PRIVATE)){
+            if (e.getMessage().getRawContent().startsWith(String.valueOf(CorgiBot.PREFIX))
+                    && !e.getAuthor().isBot()) {
+                String message = e.getMessage().getRawContent();
+                String command = message.substring(1);
+                String[] args = new String[0];
+                if (message.contains(" ")) {
+                    command = command.substring(0, message.indexOf(" ") - 1);
+
+                    args = message.substring(message.indexOf(" ") + 1).split(" ");
+                }
+                for (Command cmd : CorgiBot.getInstance().getCommandHandler().getCommands()) {
+                    if (cmd.getCommand().equalsIgnoreCase(command)) {
+                        String[] finalArgs = args;
+                        if (cmd.getType() == CommandType.WAKE) {
+                            if (!isCreator(e.getMessage().getAuthor())) {
+                                return;
+                            }
+                        } else if (cmd.getType() == CommandType.ADMINISTRATIVE) {
+                            if (!checkAdmin(e.getMessage())) {
+                                return;
+                            }
                         }
-                    } else if (cmd.getType() == CommandType.ADMINISTRATIVE) {
-                        if (!checkAdmin(e.getMessage())) {
-                            return;
+                        try {
+                            cmd.onCommand(e.getAuthor(), e.getTextChannel(), e.getMessage(), finalArgs, e.getMember());
+                        } catch (Exception ex) {
+                            MessageUtils.sendException("Chyba při provádění příkazu", ex, e.getChannel());
                         }
-                    }
-                    try {
-                        cmd.onCommand(e.getAuthor(), e.getChannel(), e.getMessage(), finalArgs, e.getMember());
-                    } catch (Exception ex) {
-                        MessageUtils.sendException("Chyba při provádění příkazu", ex, e.getChannel());
-                    }
-                    if (cmd.deleteMessage()) {
-                        delete(e.getMessage());
+                        if (cmd.deleteMessage()) {
+                            delete(e.getMessage());
+                        }
                     }
                 }
             }
+        } else {
+            e.getChannel().sendMessage(MessageUtils.getEmbed(Color.RED).setDescription(":no_entry: | Neumím zatím odpovídat na soukromé zprávy.").build()).queue();
         }
     }
 

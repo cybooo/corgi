a3085357643a8a31e4309fc7caa927e92273b893
MrWakeCZ <joseff.kral@gmail.com>
2017-06-09 17:49:33 +0200

    Přidány statistiky z Vanilla-Skyblocku

--- src/main/java/cz/wake/corgibot/commands/admin/AtsCommand.java
+++ src/main/java/cz/wake/corgibot/commands/admin/AtsCommand.java
@@ -72,8 +72,12 @@ public class AtsCommand implements Command {
             int minigames_odehrano = CorgiBot.getInstance().getSql().getStalkerStats(name, "minigames_played_time");
             long minigames_posledni_aktivita = CorgiBot.getInstance().getSql().getStalkerStatsTime(name, "minigames_pos_aktivita");
 
-            int celkem_chat = survival_chat + skyblock_chat + creative_chat + prison_chat + vanilla_chat + minigames_chat;
-            int celkem_odehrano = survival_odehrano + skyblock_odehrano + creative_odehrano + prison_odehrano + vanilla_odehrano + minigames_odehrano;
+            int vanillasb_chat = CorgiBot.getInstance().getSql().getStalkerStats(name, "vanillasb_chat_body");
+            int vanillasb_odehrano = CorgiBot.getInstance().getSql().getStalkerStats(name, "vanillasb_played_time");
+            long vanillasb_posledni_aktivita = CorgiBot.getInstance().getSql().getStalkerStatsTime(name, "vanillasb_pos_aktivita");
+
+            int celkem_chat = survival_chat + skyblock_chat + creative_chat + prison_chat + vanilla_chat + minigames_chat + vanillasb_chat;
+            int celkem_odehrano = survival_odehrano + skyblock_odehrano + creative_odehrano + prison_odehrano + vanilla_odehrano + minigames_odehrano + vanillasb_odehrano;
 
 
             channel.sendMessage(MessageUtils.getEmbed(Constants.GREEN).setTitle("ATS pro " + name)
@@ -83,6 +87,7 @@ public class AtsCommand implements Command {
                     .addField("Prison :oncoming_police_car:", "**Chat**: " + prison_chat + "\n" + "**Odehráno**: " + TimeUtils.formatTime("%d dni, %hh %mm", prison_odehrano, false) + "\n" + "**Poslední aktivita**: " + getDate(prison_posledni_aktivita), true)
                     .addField("Vanilla :rose:", "**Chat**: " + vanilla_chat + "\n" + "**Odehráno**: " + TimeUtils.formatTime("%d dni, %hh %mm", vanilla_odehrano, false) + "\n" + "**Poslední aktivita**: " + getDate(vanilla_posledni_aktivita), true)
                     .addField("MiniGames :video_game:", "**Chat**: " + minigames_chat + "\n" + "**Odehráno**: " + TimeUtils.formatTime("%d dni, %hh %mm", minigames_odehrano, false) + "\n" + "**Poslední aktivita**: " + getDate(minigames_posledni_aktivita), true)
+                    .addField("Vanilla Skyblock :jack_o_lantern:", "**Chat**: " + vanillasb_chat + "\n" + "**Odehráno**: " + TimeUtils.formatTime("%d dni, %hh %mm", vanillasb_odehrano, false) + "\n" + "**Poslední aktivita**: " + getDate(vanillasb_posledni_aktivita), true)
                     .addField("Celkem :notepad_spiral:", "**Chat**: " + celkem_chat + "\n" + "**Odehráno**: " + TimeUtils.formatTime("%d dni, %hh %mm", celkem_odehrano, false), false)
                     .setFooter("Platné pro: " + getDate(System.currentTimeMillis()), null).build()).queue();
         }

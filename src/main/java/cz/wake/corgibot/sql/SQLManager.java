3de121eae71c9661250fe7be85b6f8501fb50fee
MrWakeCZ <joseff.kral@gmail.com>
2017-10-13 13:31:30 +0200

    Oprava chybějících metod

--- src/main/java/cz/wake/corgibot/sql/SQLManager.java
+++ src/main/java/cz/wake/corgibot/sql/SQLManager.java
@@ -28,4 +28,56 @@ public class SQLManager {
         return pool;
     }
 
+    public final ResultSet getPrefixData() {
+        Connection conn = null;
+        PreparedStatement ps = null;
+        try {
+            conn = pool.getConnection();
+            ps = conn.prepareStatement("SELECT * FROM corgibot.prefixes;");
+            ps.executeQuery();
+            if (ps.getResultSet().next()) {
+                return ps.getResultSet();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            pool.close(conn, ps, null);
+        }
+        return null;
+    }
+
+    public final void deletePrefix(final String guildId) {
+        Connection conn = null;
+        PreparedStatement ps = null;
+        try {
+            conn = pool.getConnection();
+            ps = conn.prepareStatement("DELETE FROM prefixes WHERE guildid = ?");
+            ps.setString(1, guildId);
+            ps.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            pool.close(conn, ps, null);
+        }
+    }
+
+    public final void updatePrefix(final String guildId, final String prefix) {
+        Connection conn = null;
+        PreparedStatement ps = null;
+        try {
+            conn = pool.getConnection();
+            ps = conn.prepareStatement("INSERT INTO prefixes (guildid, prefix) VALUES (?, ?) ON DUPLICATE KEY UPDATE prefix = ?;");
+            ps.setString(1, guildId);
+            ps.setString(2, prefix);
+            ps.setString(3, prefix);
+            ps.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            pool.close(conn, ps, null);
+        }
+    }
+
+
+
 }

447f51c1459e824e971e248d9996793789eb58f0
MrWakeCZ <joseff.kral@gmail.com>
2018-01-06 02:30:57 +0100

    Oprava NPE u příkazu .say

--- src/main/java/cz/wake/corgibot/commands/admin/Say.java
+++ src/main/java/cz/wake/corgibot/commands/admin/Say.java
@@ -4,12 +4,17 @@ import com.jagrosh.jdautilities.waiter.EventWaiter;
 import cz.wake.corgibot.commands.ICommand;
 import cz.wake.corgibot.commands.CommandType;
 import cz.wake.corgibot.commands.Rank;
+import cz.wake.corgibot.utils.MessageUtils;
 import net.dv8tion.jda.core.entities.*;
 
 public class Say implements ICommand {
 
     @Override
     public void onCommand(User sender, MessageChannel channel, Message message, String[] args, Member member, EventWaiter w, String guildPrefix) {
+        if(args.length < 1){
+            MessageUtils.sendErrorMessage("Nelze poslat zprávu, která nemá text!", channel);
+            return;
+        }
         channel.sendMessage(message.getRawContent().replace(guildPrefix + "say", "")).queue();
     }
 

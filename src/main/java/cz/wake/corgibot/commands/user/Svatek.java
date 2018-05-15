5c66f3f6c65a7c329abb7327d3b54281baf263b6
MrWakeCZ <joseff.kral@gmail.com>
2018-05-15 03:53:51 +0200

    Přidány subpříkazy pro c!svatek
    Closes #54

--- src/main/java/cz/wake/corgibot/commands/user/Svatek.java
+++ src/main/java/cz/wake/corgibot/commands/user/Svatek.java
@@ -21,11 +21,12 @@ import org.json.JSONObject;
 @SinceCorgi(version = "1.3.0")
 public class Svatek implements Command {
 
+    //TODO: Zjednodusit API calls
+
     @Override
     public void onCommand(MessageChannel channel, Message message, String[] args, Member member, EventWaiter w, GuildWrapper gw) {
         if (args.length < 1) {
 
-            //TODO: Vcera, dneska, zitra, datum
             String czechName, slovakName;
 
             // API
@@ -48,30 +49,72 @@ public class Svatek implements Command {
                 e.getStackTrace();
             }
         } else {
-            String day = args[0];
-            String month = args[1];
+            if(args[0].equalsIgnoreCase("zitra")){
+                String czechName, slovakName;
+                OkHttpClient caller = new OkHttpClient();
+                Request request = new Request.Builder().url("https://api.abalin.net/get/tomorrow").build();
+                try {
+                    Response response = caller.newCall(request).execute();
+                    JSONObject json = new JSONObject(response.body().string());
+                    JSONObject name = json.getJSONObject("data");
 
-            if (!(ValuesUtil.isInt(day) || (ValuesUtil.isInt(month)))) {
-                MessageUtils.sendErrorMessage("Zadal jsi špatně číslo! Zkus to znova...", channel);
-            }
+                    czechName = (String) name.get("name_cz");
+                    slovakName = (String) name.get("name_sk");
 
-            OkHttpClient caller = new OkHttpClient();
-            Request request = new Request.Builder().url("https://api.abalin.net/namedays?day=" + day + "&month=" + month).build();
-            try {
-                Response response = caller.newCall(request).execute();
-                JSONObject json = new JSONObject(response.body().string());
-                JSONObject name = json.getJSONObject("data");
+                    channel.sendMessage(MessageUtils.getEmbed(Constants.BLUE).setTitle("Kdo má zítra svátek?")
+                            .setDescription(EmoteList.CZECH_FLAG + " " + czechName + "\n" +
+                                    EmoteList.SLOVAK_FLAG + " " + slovakName).build()).queue();
 
-                String czechName = (String) name.get("name_cz");
-                String slovakName = (String) name.get("name_sk");
+                } catch (Exception e) {
+                    MessageUtils.sendErrorMessage("Zřejmě chyba v API! Zkus to zachvilku :(", channel);
+                    e.getStackTrace();
+                }
+            } else if (args[0].equalsIgnoreCase("vcera")){
+                String czechName, slovakName;
+                OkHttpClient caller = new OkHttpClient();
+                Request request = new Request.Builder().url("https://api.abalin.net/get/yesterday").build();
+                try {
+                    Response response = caller.newCall(request).execute();
+                    JSONObject json = new JSONObject(response.body().string());
+                    JSONObject name = json.getJSONObject("data");
 
-                channel.sendMessage(MessageUtils.getEmbed(Constants.BLUE).setTitle("Dne " + day + "." + month + ". má svátek:")
-                        .setDescription(EmoteList.CZECH_FLAG + " " + czechName + "\n" +
-                                EmoteList.SLOVAK_FLAG + " " + slovakName).build()).queue();
+                    czechName = (String) name.get("name_cz");
+                    slovakName = (String) name.get("name_sk");
 
-            } catch (Exception e) {
-                MessageUtils.sendErrorMessage("Chyba v API nebo špatně zadaný datum!", channel);
-                e.getStackTrace();
+                    channel.sendMessage(MessageUtils.getEmbed(Constants.BLUE).setTitle("Kdo měl včera svátek?")
+                            .setDescription(EmoteList.CZECH_FLAG + " " + czechName + "\n" +
+                                    EmoteList.SLOVAK_FLAG + " " + slovakName).build()).queue();
+
+                } catch (Exception e) {
+                    MessageUtils.sendErrorMessage("Zřejmě chyba v API! Zkus to zachvilku :(", channel);
+                    e.getStackTrace();
+                }
+            } else {
+                String day = args[0];
+                String month = args[1];
+
+                if (!(ValuesUtil.isInt(day) || (ValuesUtil.isInt(month)))) {
+                    MessageUtils.sendErrorMessage("Zadal jsi špatně číslo! Zkus to znova...", channel);
+                }
+
+                OkHttpClient caller = new OkHttpClient();
+                Request request = new Request.Builder().url("https://api.abalin.net/namedays?day=" + day + "&month=" + month).build();
+                try {
+                    Response response = caller.newCall(request).execute();
+                    JSONObject json = new JSONObject(response.body().string());
+                    JSONObject name = json.getJSONObject("data");
+
+                    String czechName = (String) name.get("name_cz");
+                    String slovakName = (String) name.get("name_sk");
+
+                    channel.sendMessage(MessageUtils.getEmbed(Constants.BLUE).setTitle("Dne " + day + "." + month + ". má svátek:")
+                            .setDescription(EmoteList.CZECH_FLAG + " " + czechName + "\n" +
+                                    EmoteList.SLOVAK_FLAG + " " + slovakName).build()).queue();
+
+                } catch (Exception e) {
+                    MessageUtils.sendErrorMessage("Chyba v API nebo špatně zadaný datum!", channel);
+                    e.getStackTrace();
+                }
             }
 
         }
@@ -89,8 +132,10 @@ public class Svatek implements Command {
 
     @Override
     public String getHelp() {
-        return "%svatek - Zobrazí jména, kteří mají dnes svátek\n" +
-                "%svatek [den] [mesic] - Svátky pro konkrétní dny";
+        return "`%svatek` - Zobrazí jména, kteří mají dnes svátek\n" +
+                "`%svatek zitra` - Zobrazí kdo má zítra svátek\n" +
+                "`%svatek vcera` - Zobrazí kdo měl včera svátek\n" +
+                "`%svatek [den] [mesic]` - Svátky pro konkrétní dny";
     }
 
     @Override

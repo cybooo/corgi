a9b17ab559d8359256fd6e673030d67a7669a2ca
MrWakeCZ <joseff.kral@gmail.com>
2018-06-15 00:54:14 +0200

    Super secret!

--- src/main/java/cz/wake/corgibot/commands/user/About.java
+++ src/main/java/cz/wake/corgibot/commands/user/About.java
@@ -6,6 +6,7 @@ import cz.wake.corgibot.commands.Command;
 import cz.wake.corgibot.commands.CommandCategory;
 import cz.wake.corgibot.objects.GuildWrapper;
 import cz.wake.corgibot.utils.ColorSelector;
+import cz.wake.corgibot.utils.Constants;
 import cz.wake.corgibot.utils.MessageUtils;
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.entities.Member;
@@ -17,13 +18,24 @@ public class About implements Command {
 
     @Override
     public void onCommand(MessageChannel channel, Message message, String[] args, Member member, EventWaiter w, GuildWrapper gw) {
-        channel.sendMessage(MessageUtils.getEmbed(ColorSelector.getRandomColor()).setTitle("Představení Corgiho")
-                .setDescription("Ahoj já jsem Corgi, jsem bot pro Discord servery.\nJsem určený pro široké spektrum funkčnosti od administrace, získávání statistik z her až ke klasickým příkazům pro Discord!\n\n" +
-                        "**Web**: [https://corgibot.xyz](https://corgibot.xyz)\n" +
-                        "**Invite na tvůj server**: [Odkaz](https://discordapp.com/oauth2/authorize?client_id=294952122582302720&scope=bot&permissions=104197334)\n" +
-                        "**Vytvořil**: " + channel.getJDA().getUserById("177516608778928129").getAsMention() + "\n" +
-                        "**Suppport Guild**: [Odkaz](https://discordapp.com/invite/eaEFCYX)")
-                .setThumbnail(channel.getJDA().getSelfUser().getAvatarUrl()).build()).queue();
+        if(message.getContentRaw().contains("corgi")) {
+            channel.sendMessage(MessageUtils.getEmbed(Constants.BLUE).setTitle("Představení Corgiho")
+                    .setDescription("Ahoj já jsem Corgi, jsem bot pro Discord servery.\nJsem určený pro široké spektrum funkčnosti od administrace, získávání statistik z her až ke klasickým příkazům pro Discord!\n\n" +
+                            "**Web**: [https://corgibot.xyz](https://corgibot.xyz)\n" +
+                            "**Invite na tvůj server**: [Odkaz](https://discordapp.com/oauth2/authorize?client_id=294952122582302720&scope=bot&permissions=104197334)\n" +
+                            "**Vytvořil**: " + channel.getJDA().getUserById("177516608778928129").getAsMention() + "\n" +
+                            "**Suppport Guild**: [Odkaz](https://discordapp.com/invite/eaEFCYX)")
+                    .setThumbnail(channel.getJDA().getSelfUser().getAvatarUrl())
+                    .setImage("https://cdn.dribbble.com/users/245977/screenshots/4629944/dribbble_1.gif").build()).queue();
+        } else {
+            channel.sendMessage(MessageUtils.getEmbed(ColorSelector.getRandomColor()).setTitle("Představení Corgiho")
+                    .setDescription("Ahoj já jsem Corgi, jsem bot pro Discord servery.\nJsem určený pro široké spektrum funkčnosti od administrace, získávání statistik z her až ke klasickým příkazům pro Discord!\n\n" +
+                            "**Web**: [https://corgibot.xyz](https://corgibot.xyz)\n" +
+                            "**Invite na tvůj server**: [Odkaz](https://discordapp.com/oauth2/authorize?client_id=294952122582302720&scope=bot&permissions=104197334)\n" +
+                            "**Vytvořil**: " + channel.getJDA().getUserById("177516608778928129").getAsMention() + "\n" +
+                            "**Suppport Guild**: [Odkaz](https://discordapp.com/invite/eaEFCYX)")
+                    .setThumbnail(channel.getJDA().getSelfUser().getAvatarUrl()).build()).queue();
+        }
     }
 
     @Override
@@ -48,6 +60,6 @@ public class About implements Command {
 
     @Override
     public String[] getAliases() {
-        return new String[]{"info", "binfo"};
+        return new String[]{"info", "binfo", "corgi"};
     }
 }
